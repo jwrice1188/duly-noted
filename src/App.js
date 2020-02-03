0808001b78e9341df899820854611e97535b4878
James <jwrice1188@gmail.com>
2020-02-03 16:01:04 -0600

    Restarts Project

--- src/App.js
+++ src/App.js
@@ -1,13 +1,24 @@
 import React from 'react';
-import NoteListPage from './components/NoteListPage';
-import NoteEditPage from './components/NoteEditPage';
+import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <NoteListPage />
-      {/* <NoteEditPage text="React is fun prop!" /> */}
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Duly Noted
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
 }
